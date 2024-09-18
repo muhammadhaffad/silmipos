e6ad193b5340464dfe1319c04ab0316aa30598cb
hadap <robottxt5@gmail.com>
2024-09-18 16:01:57 +0700

    tambah logic store produk, TODO: logic update harga produk

---
+++ query log 3.sql
@@ -0,0 +1,20 @@
+﻿create sequence toko_griyanaura.ms_produkvarian_kode_produkvarian_seq
+	start 1
+	no cycle;
+
+create or replace function toko_griyanaura.get_kodeprodukvarian()
+	returns text
+as $$
+declare 
+	p_val int; 
+	v_ret text;
+begin
+	p_val := nextval('toko_griyanaura.ms_produkvarian_kode_produkvarian_seq'::regclass);
+	v_ret := 'P' || lpad(p_val::text, 6, '0');
+
+	return v_ret;
+end;
+$$
+language plpgsql;
+
+alter table toko_griyanaura.ms_produkvarian alter column kode_produkvarian set default toko_griyanaura.get_kodeprodukvarian();
