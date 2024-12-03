b3684dbddee9da861469c68dfe447eaf892e8796
hadap <robottxt5@gmail.com>
2024-12-03 16:02:37 +0700

    TODO: data penjualan, insert relationshipe manager

---
+++ app/Filament/PointOfSale/Resources/SalesInvoiceResource/RelationManagers/PenjualanBayarRelationManager.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\PointOfSale\Resources\SalesInvoiceResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PenjualanBayarRelationManager extends RelationManager
+{
+    protected static string $relationship = 'penjualanBayar';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('nominal')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('nominal')
+            ->columns([
+                Tables\Columns\TextColumn::make('nominal'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
