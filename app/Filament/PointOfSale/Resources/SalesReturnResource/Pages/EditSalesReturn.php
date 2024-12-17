07e478e049107c4d619fc0ed28455c3be9e44f40
hadap <robottxt5@gmail.com>
2024-12-17 16:02:26 +0700

    fix bug record retur sales

--- app/Filament/PointOfSale/Resources/SalesReturnResource/Pages/EditSalesReturn.php
+++ app/Filament/PointOfSale/Resources/SalesReturnResource/Pages/EditSalesReturn.php
@@ -3,6 +3,7 @@
 namespace App\Filament\PointOfSale\Resources\SalesReturnResource\Pages;
 
 use App\Filament\PointOfSale\Resources\SalesReturnResource;
+use App\Services\Core\Sales\SalesReturnService;
 use Filament\Actions;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
@@ -10,6 +11,7 @@ use Filament\Resources\Pages\EditRecord;
 use Filament\Forms\Components;
 use Awcodes\TableRepeater;
 use Filament\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Support\Enums\Alignment;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
@@ -22,17 +24,36 @@ class EditSalesReturn extends EditRecord
 
     public static string|Alignment $formActionsAlignment = Alignment::End;
 
+    protected $salesReturnService;
+
+    public function __construct()
+    {
+        $this->salesReturnService = new SalesReturnService;    
+    }
+
     public function mount(int|string $record): void
     {
         parent::mount($record);
-        $this->record->load(['penjualanReturDetail', 'penjualan.penjualanDetail' => function ($query) {
-            $query->with(['produkVarian']);
-            $query->whereHas('produkVarian.produk', function ($q2) {
-                $q2->where('in_stok', true);
-            });
-            $query->leftJoin(DB::raw("(select id_penjualandetail as id_penjualandetaildiretur, sum(qty) as jumlah_diretur from toko_griyanaura.tr_penjualanreturdetail group by id_penjualandetail) as x"), 'x.id_penjualandetaildiretur', 'toko_griyanaura.tr_penjualandetail.id_penjualandetail');
-        }]);
-        $this->fillForm();
+        \Filament\Support\Facades\FilamentView::registerRenderHook(\Filament\View\PanelsRenderHook::STYLES_AFTER, function () {
+            return '<style>' . <<<'CSS'
+                    .table-repeater-container {
+                        overflow-x: scroll;
+                    }
+                    .table-repeater-container tr td {
+                        vertical-align: top;
+                    }
+                    .table-repeater-container td:nth-child(1), .table-repeater-container th:nth-child(1) {
+                        position: sticky;
+                        z-index: 10;
+                        left: 0px;
+                    }
+                    .table-repeater-container td:last-child, .table-repeater-container th:last-child {
+                        position: sticky;
+                        z-index: 10;
+                        right: 0px;
+                    }
+            CSS . '</style>';
+        });
     }
 
     protected function getCancelFormAction(): Action
@@ -67,7 +88,6 @@ class EditSalesReturn extends EditRecord
 
     public function form(Form $form): Form
     {
-        $returItems = $this->record->penjualanReturDetail->keyBy('id_penjualandetail');
         return parent::form($form)
             ->schema([
                 Section::make()
@@ -124,20 +144,21 @@ class EditSalesReturn extends EditRecord
                                         Components\TextInput::make('produk_varian.varian')
                                             ->disabled()
                                             ->hiddenLabel(),
-                                        Components\Placeholder::make('')
-                                            ->content(function ($get) use ($returItems) {
-                                                return ($get('jumlah_diretur') ? (number($get('jumlah_diretur')) - number($returItems[$get('id_penjualandetail')]?->qty ?? null)) : '0') . '/' . number($get('qty'));
-                                            }),
+                                        Components\Placeholder::make('jumlah_diretur')
+                                            ->content(function ($get) {
+                                                return (($get('jumlah_diretur') - ($get('../../../returnItems')[$get('id_penjualandetail')]['qty'] ?? 0))) . '/' . number($get('qty'));
+                                            })
+                                            ->hiddenLabel(),
                                         Components\TextInput::make('qty_diretur')
                                             ->live(debounce: 500)
-                                            ->formatStateUsing(function ($get) use ($returItems) {
-                                                return number($returItems[$get('id_penjualandetail')]?->qty ?? null);
+                                            ->formatStateUsing(function ($get) {
+                                                return number($get('../../../returnItems')[$get('id_penjualandetail')]['qty'] ?? null);
                                             })
                                             ->afterStateUpdated(function ($get, $set) {
                                                 $set('total', (int)((float)$get('qty_diretur') * (int)$get('harga') * (1-(float)$get('diskon')/100)));
                                             })
-                                            ->maxValue(function ($get) use ($returItems) {
-                                                return ($get('jumlah_diretur') - number($returItems[$get('id_penjualandetail')]?->qty ?? null)) + $get('qty');
+                                            ->maxValue(function ($get) {
+                                                return $get('qty') - ($get('jumlah_diretur') - ($get('../../../returnItems')[$get('id_penjualandetail')]['qty'] ?? 0));
                                             })
                                             ->minValue(0)
                                             ->numeric(),
@@ -162,6 +183,10 @@ class EditSalesReturn extends EditRecord
                                             ->extraInputAttributes([
                                                 'class' => 'text-end'
                                             ]),
+                                        Components\Hidden::make('id_penjualanreturdetail')
+                                            ->formatStateUsing(function ($get) {
+                                                return $get('../../../returnItems')[$get('id_penjualandetail')]['id_penjualanreturdetail'] ?? null;
+                                            }),
                                     ])
                                     ->addable(false)
                                     ->deletable(false)
@@ -228,17 +253,67 @@ class EditSalesReturn extends EditRecord
                                     ])
                             ])
                     ])
+                    ->extraAttributes([
+                        'class' => '
+                        [&_.table-repeater-container_td:nth-child(1)]:dark:!bg-gray-900 
+                        [&_.table-repeater-container_th:nth-child(1)]:dark:!bg-gray-900 
+                        [&_.table-repeater-container_td:last-child]:dark:!bg-gray-900 
+                        [&_.table-repeater-container_th:last-child]:dark:!bg-gray-900
+                        [&_.table-repeater-container_td:nth-child(1)]:!bg-white 
+                        [&_.table-repeater-container_th:nth-child(1)]:!bg-gray-100 
+                        [&_.table-repeater-container_td:last-child]:!bg-white
+                        [&_.table-repeater-container_th:last-child]:!bg-gray-100 
+                        [&_.table-repeater-container_td:nth-child(1)>*]:md:!w-[220px] 
+                        [&_.table-repeater-container_td:nth-child(2)>*]:md:!w-[100px] 
+                        [&_.table-repeater-container_td:nth-child(3)>*]:md:!w-[100px] 
+                        [&_.table-repeater-container_td:nth-child(4)>*]:md:!w-[150px] 
+                        [&_.table-repeater-container_td:nth-child(5)>*]:md:!w-[100px] 
+                        [&_.table-repeater-container_td:nth-child(6)>*]:md:!w-[150px] 
+                        [&_.table-repeater-container_td:nth-child(7)>*]:md:!w-[40px]'
+                    ])
             ]);
     }
 
     protected function mutateFormDataBeforeFill(array $data): array
     {
+        $this->record->refresh();
+        $this->record->load(['penjualanReturDetail', 'penjualan.penjualanDetail' => function ($query) {
+            $query->with(['produkVarian']);
+            $query->whereHas('produkVarian.produk', function ($q2) {
+                $q2->where('in_stok', true);
+            });
+            $query->leftJoin(DB::raw("(select id_penjualandetail as id_penjualandetaildiretur, sum(qty) as jumlah_diretur from toko_griyanaura.tr_penjualanreturdetail group by id_penjualandetail) as x"), 'x.id_penjualandetaildiretur', 'toko_griyanaura.tr_penjualandetail.id_penjualandetail');
+        }]);
+        $this->record['returnItems'] = $this->record->penjualanReturDetail->keyBy('id_penjualandetail')->toArray();
         return $this->record->toArray();
     }
 
     protected function handleRecordUpdate(Model $record, array $data): Model
     {
-        dump($record, $data);
-        return $record;
+        $data['penjualanDetail'] = array_map(function ($item) {
+            return [
+                'qty_diretur' => $item['qty_diretur'],
+                'id_penjualanreturdetail' => $item['id_penjualanreturdetail'],
+                'id_penjualandetail' => $item['id_penjualandetail']
+            ];
+        }, $data['penjualan']['penjualan_detail']);
+        try {
+            $salesReturn = $this->salesReturnService->updateReturn($this->record->id_penjualanretur, $data);
+            $this->fillForm();
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            Notification::make()
+                ->title('422 Unprocessable Entity, please contact developer.')
+                ->danger()
+                ->send();
+            $this->halt();
+        } catch (\Exception $e) {
+            Notification::make()
+                ->title('500 Internal Server Error, please contact developer.')
+                ->body($e->getMessage())
+                ->danger()
+                ->send();
+            $this->halt();
+        }
+        return $salesReturn;
     }
 }
